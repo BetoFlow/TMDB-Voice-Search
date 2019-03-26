bc546a9a7658507db44d90ccff0bb469d23603d2
Alberto Miguens <43731470+BetoFlow@users.noreply.github.com>
2019-03-25 22:55:17 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo of Dialogflow with Mozilla Web Speech API
+
+This is a demo of using Dialogflow intents with Mozilla Web Speech API streaming speech to text in real-time from a browser.
---
+++ server.js
@@ -0,0 +1,62 @@
+
+var path = require('path');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+// You can find your project ID in your Dialogflow agent settings
+const projectId = 'my-weather-55a85'; //https://dialogflow.com/docs/agents#settings
+const sessionId = 'quickstart-session-id';
+const languageCode = 'es-US';
+
+// Instantiate a DialogFlow client.
+const dialogflow = require('dialogflow');
+const sessionClient = new dialogflow.SessionsClient();
+
+// Define Dialogflow session path
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+io.on('connection', function(socket) {
+  console.log('Connected to browser...');
+  socket.on('final_transcript', function(final_transcript) {
+    runDF(final_transcript)
+  });
+});
+
+var runDF = function(data) {
+  var requestDF = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: data,
+        languageCode: languageCode,
+      },
+    },
+  }
+  sessionClient
+    .detectIntent(requestDF)
+    .then(function(responses) {
+      const result = responses[0].queryResult;
+      // console.log(result.parameters.fields.quantity.numberValue)
+      console.log(`  Query: ${result.queryText}`);
+      console.log(`  Response: ${result.fulfillmentText}`);
+      console.log(`  Parameters: ${result.parameters.fields.quantity}`);
+      if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+      } else {
+        console.log(`  No intent matched.`);
+      }
+    })
+    .catch(function(err) {
+      console.error('ERROR:', err);
+    })
+}
+
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('./index.html'));
+});
+
+server.listen(process.env.PORT || 8080, function() {
+	console.log("Node server started...")
+});
